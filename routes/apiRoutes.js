612708c65304cff43e9aa15ffdde60b6f4a2fb00
Travis Brown <bergannation@gmail.com>
2021-06-01 13:24:03 -0500

    added static so the pages were functioning correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,10 @@
 
 let newNotes = require("../db/newNotes");
 
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // ROUTING
 
 module.exports = (app) => {
@@ -10,5 +14,17 @@ module.exports = (app) => {
 
   app.post("/api/notes", (req, res) => {
     let newNote = req.body;
+
+    //
+    console.log("Req.body:", newNote);
+    newNotes.push(newNote);
+
+    writeFileAsync("./db/newNotes.json", JSON.stringify(newNotes)).then(
+      function () {
+        console.log("Update made to newNotes.json");
+      }
+    );
+
+    res.json(newNote);
   });
 };
